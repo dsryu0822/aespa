d54d72a63f5c39c4963558139f68067ab9bbb7e7
rmsmsgood <rmsmsgood@naver.com>
2021-02-17 14:19:26 +0900

    접촉 방식 롤백

--- aespa.jl
+++ aespa.jl
@@ -1,37 +1,44 @@
-@time using BenchmarkTools
+# @time using BenchmarkTools
 @time using Profile
 @time using Statistics
+@time using Random
 @time using Base.Threads
 # @time using Dates
-@time using Random
+@time using Distances
 @time using LinearAlgebra
 @time using LightGraphs
 @time using Distributions
 @time using Plots
 
-test = false
+test = true
 visualization = false
 
 # ------------------------------------------------------------------
 
 # variables
-t = 0 # micro timestep
 T = 0 # Macro timestep
 
 # parameters
-n = 10^6 # number of agent
-N = n ÷ 500 # number of stage network
+n = 10^5 # number of agent
+N = n ÷ 1000 # number of stage network
 m = 3 # number of network link
 
-β = 0.02 # infection rate
-invε = 10 # inverse-epsilon
+α = 0.3
+β = 0.01 # infection rate
+# invε = 10 # inverse-epsilon
+ε² = 0.0025
 
-fear_threshold = 1000
-fear_factor = 20
+fear_threshold = 100
+fear_factor = 10
 
 brownian = MvNormal(2, 0.05) # moving process
 incubation_period = Weibull(3, 7.17) # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7014672/#__sec2title
 recovery_period = Weibull(3, 7.17)
+noise = Cauchy(0,1)
+
+for t in 1:20
+    println(mean(rand(noise, 100,100)))
+end
 
 # ------------------------------------------------------------------
 
@@ -49,13 +56,13 @@ PERSONAL_ = Array{Float64, 1}()
 VALUE_ = Array{Float64, 1}()
 
 ID = 1:n
-PUBLIC = 50 # Public score
+# PUBLIC = 50 # Public score
 protective = 0.1
 neutral    = 0.5
 aggressive = 0.9
 
-reward_day = -7
-reward_move = 3
+reward_day = -11
+reward_move = 5
 
 # ------------------------------------------------------------------
 
@@ -75,7 +82,7 @@ value = personal = rand(1:100, n)
 INCUBATION = zeros(Int64, n) .- 1
   RECOVERY = zeros(Int64, n) .- 1
 
-G = barabasi_albert(N, m)
+G = barabasi_albert(N, m); NODE = G.fadjlist
 
 state = Array{Char, 1}(undef, n); state .= 'S' # using SEIR model
 policy = Array{Char, 1}(undef, n); policy .= 'N' # 'A': Aggressive, 'N': Neutral, 'P': Protective, 'R': Removed
@@ -94,25 +101,26 @@ location_pixel = zeros(Int64, 2, n)
     INCUBATION .-= 1
     RECOVERY .-= 1
     state[INCUBATION .== 0] .= 'I'
-    state[RECOVERY .== 0] .= 'R'
+    bit_RECOVERY = (RECOVERY .== 0)
+    state[bit_RECOVERY] .= 'R'
+    personal[bit_RECOVERY] .= 0
 
-    personal = min.(100, max.(0, personal .+ reward_day))
+    personal = personal .+ reward_day
 
-    # value = (state .!= 'R') .*
-    #  (((1 .+ (L .< personal) .+ (U .< personal)) .* personal) .+ value) ./ 2
     n_I = sum(state .== 'I')
-    value = (state .!= 'R') .* (value .+ (
-        personal .+ 
-        # fear_factor*(n_I > 100) .+
-        fear_factor*(n_I > 1000)
-        )) ./ 2
+    value = ((1 - α) * value) .+ (α * personal) .+ fear_factor*(n_I > fear_threshold) 
+    # value = (state .!= 'R') .* (value .+ (
+    #     personal .+ 
+    #     # fear_factor*(n_I > 100) .+
+    #     fear_factor*(n_I > 1000)
+    #     )) ./ 2
 
-    policy1 =       value .== 0
-    policy2 =  0 .< value .≤  L
+    policy[bit_RECOVERY] .= 'R'
+    policy2 =       value .≤  L
     policy3 =  L .< value .≤  U
     policy4 =  U .< value
 
-    policy[policy1] .= 'R'
+    # policy[policy1] .= 'R'
     policy[policy2] .= 'A'
     policy[policy3] .= 'N'
     policy[policy4] .= 'P'
@@ -126,7 +134,7 @@ location_pixel = zeros(Int64, 2, n)
             if LOCATION[id] == 0
                 LOCATION[id] = rand(1:N)
             else
-                LOCATION[id] = rand(G.fadjlist[LOCATION[id]])
+                LOCATION[id] = rand(NODE[LOCATION[id]]) # 이것도 스태틱하게 딱 저장해서 할 수 있을듯
             end
             personal[id] += reward_move
         else
@@ -134,59 +142,78 @@ location_pixel = zeros(Int64, 2, n)
         end
     end
 
-    ID_staged = ID[(LOCATION .> 0)]
-    size_staged = length(ID_staged)
+    bit_staged = (LOCATION .> 0)
+    ID_staged = ID[bit_staged]
+    n_staged = length(ID_staged)
 
     push!(S_, sum(state .== 'S'))
     push!(E_, sum(state .== 'E'))
     push!(I_, sum(state .== 'I'))
     push!(R_, sum(state .== 'R'))
 
-    push!(Re_, sum(policy1))
+    # push!(Re_, sum(policy1))
     push!(Ag_, sum(policy2))
     push!(Ne_, sum(policy3))
     push!(Pr_, sum(policy4))
 
     push!(VALUE_, mean(value))
     push!(PERSONAL_, mean(personal))
-    println("T: $T - Staged: $size_staged | $(S_[T]) | $(E_[T]) | $(I_[T]) | $(R_[T]) | Value: $(PERSONAL_[T]) |")
-    println("      Aggressive $(Ag_[T]) | Neutral: $(Ne_[T]) | Protective: $(Pr_[T]) |")
+    print("T: $T - Staged: $n_staged | S: $(S_[T]) | E: $(E_[T]) | I: $(I_[T]) | R:$(R_[T])")
+    println(" | Personal: $(PERSONAL_[T]) | Aggressive $(Ag_[T]) | Neutral: $(Ne_[T]) | Protective: $(Pr_[T]) |")
 
     for t in 1:8
-        for id in ID_staged
-            if rand() < decision[id]
-                LOCATION[id] = rand(G.fadjlist[LOCATION[id]])
-                personal[id] += reward_move
+        bit_macro_S = (state .== 'S')
+        bit_macro_I = (state .== 'I')
+
+        moved = ID_staged[rand(n_staged) .< decision[ID_staged]]
+        LOCATION[moved] = rand.(NODE[LOCATION[moved]])
+        personal[moved] .+= reward_move
+
+        # for id in ID_staged
+        #     if rand() < decision[id]
+        #         LOCATION[id] = rand(NODE[LOCATION[id]])
+        #         personal[id] += reward_move
+        #     end
+        # end
+
+        NODE_I = setdiff(unique(LOCATION[bit_macro_I]), 0)
+        if length(NODE_I) ≥ 40
+            @threads for node in NODE_I
+                bit_node = (LOCATION .== node)
+                bit_S = bit_node .& bit_macro_S
+                bit_I = bit_node .& bit_macro_I
+                ID_S = ID[bit_S]
+                ID_I = ID[bit_I]
+
+                location[:,ID_S] = mod.(location[:,ID_S] + rand(brownian, sum(bit_S)), 1.0)
+                location[:,ID_I] = mod.(location[:,ID_I] + rand(brownian, sum(bit_I)), 1.0)
+
+                contact = vec(sum(pairwise(SqEuclidean(),location[:,ID_S],location[:,ID_I]) .< ε², dims=2))
+                bit_infected = rand(sum(bit_S)) .< (1 .- (1 - β).^contact)
+                ID_infected = ID_S[bit_infected]
+                
+                state[ID_infected] .= 'E'
+                INCUBATION[ID_infected] .= round.(rand(incubation_period, sum(bit_infected)))
+                RECOVERY[ID_infected] .= INCUBATION[ID_infected] + round.(rand(recovery_period, sum(bit_infected)))
             end
-        end
-
-        @threads for node in setdiff(unique(LOCATION[state .== 'I']), 0)
-            # if node == 0 continue end
-            bit_S = (LOCATION .== node) .& (state .== 'S')
-            bit_I = (LOCATION .== node) .& (state .== 'I')
-            ID_S = ID[bit_S]
-            ID_I = ID[bit_I]
-
-            location[:,ID_S] += rand(brownian, sum(bit_S))
-            location[:,ID_I] += rand(brownian, sum(bit_I))
-
-            location_pixel[:,ID_S] = Int64.(ceil.(invε .* location[:,ID_S]))
-            location_pixel[:,ID_I] = Int64.(ceil.(invε .* location[:,ID_I]))
-
-            for from in shuffle(ID_I)
-                contated = 
-                (location_pixel[1,from] .== location_pixel[1,ID_S]) .&
-                (location_pixel[2,from] .== location_pixel[2,ID_S])
-                if test print("$from → ") end
-                for to in ID_S[contated]
-                    if rand() < β
-                        if test print("$to, ") end
-                        state[to] = 'E'
-                        INCUBATION[to] = round(rand(incubation_period))
-                        RECOVERY[to] = INCUBATION[to] + round(rand(recovery_period))
-                    end
-                end
-                if test println() end
+        else
+            for node in NODE_I
+                bit_node = (LOCATION .== node)
+                bit_S = bit_node .& bit_macro_S
+                bit_I = bit_node .& bit_macro_I
+                ID_S = ID[bit_S]
+                ID_I = ID[bit_I]
+    
+                location[:,ID_S] = mod.(location[:,ID_S] + rand(brownian, sum(bit_S)), 1.0)
+                location[:,ID_I] = mod.(location[:,ID_I] + rand(brownian, sum(bit_I)), 1.0)
+    
+                contact = vec(sum(pairwise(SqEuclidean(),location[:,ID_S],location[:,ID_I]) .< ε², dims=2))
+                bit_infected = rand(sum(bit_S)) .< (1 .- (1 - β).^contact)
+                ID_infected = ID_S[bit_infected]
+                
+                state[ID_infected] .= 'E'
+                INCUBATION[ID_infected] .= round.(rand(incubation_period, sum(bit_infected)))
+                RECOVERY[ID_infected] .= INCUBATION[ID_infected] + round.(rand(recovery_period, sum(bit_infected)))
             end
         end
     end
@@ -197,7 +224,6 @@ mean(U[state .== 'R'])
 
 mean(L[state .!= 'R'])
 mean(U[state .!= 'R'])
-
 
 # if visualization
 plot_score = plot(PERSONAL_, label = "personal", color= :blue,
@@ -213,8 +239,8 @@ plot_policy = plot(Re_, label = "Re", color= :black,
 savefig(plot_policy, "plot_policy.png")
 
 
-plot_EI = plot(E_, label = "E", color= :red, linestyle = :dash,
- size = (400, 300), dpi = 300, legend=:topleft)
+plot_EI = plot(E_, label = "E", color= :orange, linestyle = :dash,
+ size = (400, 300), dpi = 300, legend=:topright)
  plot!(I_, label = "I", color= :red)
  xlabel!("T"); ylabel!("#")
 savefig(plot_EI, "plot_EI.png")
@@ -223,14 +249,3 @@ plot_R = plot(R_, label = "R", color= :black,
  size = (400, 300), dpi = 300, legend=:topleft)
  xlabel!("T"); ylabel!("#")
 savefig(plot_R, "plot_R.png")
-# end
-
-# using ProfileView
-# ProfileView.view()
-# plot!(figure[2], sol,vars=(0,1),
-# linestyle = :dash,  color = :black,
-# label = "Theoretical", legend=:topleft)
-# plot!(figure[2], 0.0:0.01:t, time_evolution,
-# color = RGB(1.,94/255,0.), linewidth = 2, label = "Simulation",
-# yscale = :log10, yticks = 10 .^(1:4))
-# ylims!(figure[2], 0., 100.)
