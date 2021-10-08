c70093ddc3eabe12941da33955d14b1b03261ab5
rmsmsgood <rmsmsgood@naver.com>
2021-10-08 18:49:31 +0900

    백신 구현 전 백업

--- aespa.jl
+++ aespa.jl
@@ -17,7 +17,7 @@ function deep_pop!(array)
 end
 
 # ------------------------------------------------------------------ parameters
-const n = 100_000
+const n = 1*10^6
 N = n ÷ 1000 # number of stage network
 ID = 1:n
 const m = 3 # number of network link
@@ -25,24 +25,28 @@ const number_of_host = 1
 const end_time = 100
 
 const β = 0.001
-const δ = σ = 0.05
+const B = 10 # vaccinated agents are infected with probability β/B
+const δ = 0.05
+const σ = 0.05
 
 brownian = MvNormal(2, 0.01) # moving process
 incubation_period = Weibull(3, 7.17) # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7014672/#__sec2title
 recovery_period = Weibull(3, 7.17)
 
 # ------------------------------------------------------------------ Random Setting
-SEED = 1:6
+SEED = 1:10
 ensemble = Int64[]
 # seed_number = 0
 for seed_number ∈ SEED
 Random.seed!(seed_number); println(seed_number)
 NODE = barabasi_albert(N, m).fadjlist
+# NODE = erdos_renyi(N, 0.02).fadjlist
 
 S_ = Int64[]
 E_ = Int64[]
 I_ = Int64[]
 R_ = Int64[]
+V_ = Int64[]
 daily_ = Int64[]
 
 NODE_I_ = zeros(Int64, end_time, N)
@@ -74,15 +78,17 @@ localtrajectory = DataFrame(
     num_to = 0
     )
 R₀_table = DataFrame(
-    T = 0,
-    t = 0,
-    node_id = 0,
-    degree = 0,
-    from = 0,
-    to = 0
+    T = Int64[],
+    t = Int64[],
+    node_id = Int64[],
+    # degree = Int64[],
+    x = Float16[],
+    y = Float16[],
+    from = Int64[],
+    to = Int64[]
 )
 delete!(localtrajectory, 1)
-delete!(R₀_table, 1)
+# delete!(R₀_table, 1)
 
 
 @time while sum(state .== 'E') + sum(state .== 'I') > 0
@@ -99,6 +105,7 @@ delete!(R₀_table, 1)
     bit_E = (state .== 'E'); n_E = count(bit_E); push!(E_, n_E)
     bit_I = (state .== 'I'); n_I = count(bit_I); push!(I_, n_I)
     bit_R = (state .== 'R'); n_R = count(bit_R); push!(R_, n_R)
+    bit_V = (state .== 'R'); n_V = count(bit_V); push!(R_, n_V)
 
     if T > 0
         println("$T: |E: $(E_[T]) |I: $(I_[T]) |R:$(R_[T])")
@@ -110,40 +117,48 @@ delete!(R₀_table, 1)
     NODE_I = unique(LOCATION[bit_I])
     for node in NODE_I
         bit_node = (LOCATION .== node)
+
         bit_micro_S = bit_node .& bit_S
+        bit_micro_E = bit_node .& bit_E
         bit_micro_I = bit_node .& bit_I
+        bit_micro_V = bit_node .& bit_V
 
         ID_S = ID[bit_micro_S]; n_micro_S = count(bit_micro_S)
+        ID_E = ID[bit_micro_E]; n_micro_E = count(bit_micro_E)
         ID_I = ID[bit_micro_I]; n_micro_I = count(bit_micro_I)
+        ID_V = ID[bit_micro_V]; n_micro_V = count(bit_micro_V)
+
         NODE_I_[T, node] = n_micro_I
 
-        bit_infected = zeros(Bool,n_micro_S)
-        ID_infected = zeros(Int64,n_micro_S)
-        in_δ = zeros(Int64,n_micro_S)
-        n_infected = 0
+        # bit_infected = zeros(Bool, n_micro_S)
+        # ID_infected = zeros(Int64, n_micro_S)
+        # in_δ = zeros(Int64, n_micro_S)
+        # n_infected = 0
         for t in 1:24
             coordinate[:,ID_S] = mod.(coordinate[:,ID_S] + rand(brownian, n_micro_S), 1.0)
+            coordinate[:,ID_E] = mod.(coordinate[:,ID_E] + rand(brownian, n_micro_E), 1.0)
             coordinate[:,ID_I] = mod.(coordinate[:,ID_I] + rand(brownian, n_micro_I), 1.0)
+            coordinate[:,ID_V] = mod.(coordinate[:,ID_V] + rand(brownian, n_micro_V), 1.0)
             
             kdtreeI = KDTree(coordinate[:,ID_I])
+
+            ## transmission I to S
             in_δ = inrange(kdtreeI, coordinate[:,ID_S], δ)
             contact = length.(in_δ)
             bit_infected =  (rand(n_micro_S) .< (1 .- (1 - β).^contact))
-            # bit_infected =  bit_infected .| (rand(n_micro_S) .< (1 .- (1 - β).^contact))
-            # if length(ID_I) == 1
-            #     one = ID_I[1]
-            #     push!(localtrajectory,[lpad(one,5,'0'), LOCATION[one], T, t, coordinate[1, one], coordinate[2, one], count(bit_infected)])
-            # end
             ID_infected = ID_S[bit_infected]
             
             n_infected = count(bit_infected)
             if n_infected > 0
+                from_id = ID_I[deep_pop!.(shuffle.(in_δ))[bit_infected]]
                 append!(R₀_table, DataFrame(
                     T = T,
                     t = t,
                     node_id = node,
-                    degree = length(NODE[node]),
-                    from = ID_I[deep_pop!.(shuffle.(in_δ))[bit_infected]],
+                    # degree = length(NODE[node]),
+                    x = coordinate[1,from_id],
+                    y = coordinate[2,from_id],
+                    from = from_id,
                     to = ID_infected
                     ))
             end
@@ -156,11 +171,11 @@ end
 
 if R_[end] > 1000
     push!(ensemble, R_[end])
-    time_evolution = DataFrame(hcat(S_, E_, I_, R_), ["S", "E", "I", "R"])
-    time_evolution_nodewise = DataFrame(NODE_I_, :auto)
+    time_evolution = DataFrame(hcat(S_, E_, I_, R_, V_), ["S", "E", "I", "R", "V"])
+    time_evolution_nodewise = DataFrame(NODE_I_)
     CSV.write(directory * "$seed_number time_evolution.csv", time_evolution)
     CSV.write(directory * "$seed_number time_evolution_nodewise.csv", time_evolution_nodewise)
-    CSV.write(directory * "$seed_number localtrajectory.csv", localtrajectory)
+    # CSV.write(directory * "$seed_number localtrajectory.csv", localtrajectory)
     unique!(R₀_table, :to)
     CSV.write(directory * "$seed_number R0_table.csv", R₀_table)
     
