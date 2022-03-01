71650925f215f1375f937cfd5552f45293793580
rmsmsgood <rmsmsgood@naver.com>
2022-03-02 00:57:09 +0900

    리얼 데이터 시뮬레이션 구현 완료

--- aespa.jl
+++ aespa.jl
@@ -6,6 +6,7 @@ schedule = DataFrame(XLSX.readtable(excuted_DIR * "/schedule.xlsx", "schedule").
 @time using Crayons
 @time using Random, Distributions, Statistics, StatsBase
 @time using Graphs, NearestNeighbors
+@time using Plots
 
 @time include("src/lemma.jl")
 @time include("src/main.jl")
@@ -22,17 +23,17 @@ cd(root); pwd()
 
 export_type = :CSV # :both, :CSV, :XLSX
 if isempty(ARGS)
-    doing = 300
+    doing = 1
 else
     doing = parse(Int64, ARGS[1])
 end
 scenario = schedule[doing,:]
 try
     mkpath(root * scenario.name)
-    mkpath(root * scenario.name * "/00")
-    mkpath(root * scenario.name * "/0V")
-    mkpath(root * scenario.name * "/M0")
-    mkpath(root * scenario.name * "/MV")
+    # mkpath(root * scenario.name * "/00")
+    # mkpath(root * scenario.name * "/0V")
+    # mkpath(root * scenario.name * "/M0")
+    # mkpath(root * scenario.name * "/MV")
     CSV.write(root * scenario.name * "/cnfg.csv", DataFrame(scenario), bom = true)
     cd(root * scenario.name)
 catch
@@ -46,56 +47,38 @@ global θ = 10
 
 global network = scenario.network
 global σ = scenario.σ
-global e_σ = scenario.e_σ
-global p_V = scenario.p_V
-global e_V = scenario.e_V
+# global e_σ = scenario.e_σ
+# global p_V = scenario.p_V
+# global e_V = scenario.e_V
 global β = scenario.β
 global n = scenario.n
-global δ = scenario.δ
 global end_time = scenario.end_time
 global first_seed = scenario.first_seed
 global last_seed = scenario.last_seed
 
-global ID = 1:n
-global brownian = MvNormal(2, δ) # moving process
-
-if network == 0
-    global metapopulation = :euler
-    global N = scenario.N
-    global m = 3 # number of network link
-    backbone = barabasi_albert(N, m, seed = 0)
-    global NODE = Dict(1:N .=> backbone.fadjlist)
-    CSV.write(root * "ntwk.csv", DataFrame(
-        deg = degree(backbone),
-        betweenness = betweenness_centrality(backbone),
-        closeness = closeness_centrality(backbone),
-        stress = stress_centrality(backbone)
-        ))
-elseif network == -1
-    global metapopulation = :lagrange
-    using JLD
-    realnetwork = load(excuted_DIR * "/world_wide_flight_network.jld")
-    global NODE = Dict(realnetwork["nodes"] .=> realnetwork["adj"])
-    global N = length(NODE)
-elseif network == -2
-    global metapopulation = :swap
-    using JLD
-    realnetwork = load(excuted_DIR * "\\world_wide_data-2.jld")
-    global NODE = Dict(realnetwork["nodes"] .=> realnetwork["adj"])
-    global area = Dict(realnetwork["nodes"] .=> sqrt.(realnetwork["area"])/100)
-    global N = length(NODE)
-    global initial_population = realnetwork["population"]
-end
-global NODE_ID = convert.(String,keys(NODE)) |> sort
-
 global latent_period = Weibull(3, 7.17) # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7014672/#__sec2title
 global recovery_period = Weibull(3, 7.17)
 
-@time for v ∈ [true, false], m ∈ [true, false]
-    for seed_number ∈ first_seed:last_seed
+global ID = 1:n
+global δ = 0.05
+global brownian = MvNormal(2, δ) # moving process
+
+if network == "data"
+    using JLD
+    realnetwork = load(excuted_DIR * "\\data_link.jld")
+    global NODE = realnetwork["adj_encoded"]
+    
+    data = CSV.read(excuted_DIR * "\\data_node.csv",DataFrame)
+    global N = nrow(data)    
+    global NODE_ID = 1:N
+    global XY = vcat(data.Longitude', data.Latitude')
+    global city = data.City
+    global country = data.Country
+    global iata = data.IATA
+    default(markerstrokewidth = 0, alpha = 0.5, markersize = 3, size = (800,400))
+end
+
+@time for seed_number ∈ first_seed:last_seed
         simulation(
-        seed_number,
-        moving = m,
-        vaccin = v)
-    end
+        seed_number)
 end
