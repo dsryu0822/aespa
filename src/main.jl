9e1545c849e46f4eef27c0f57265abda8eb55ad7
rmsmsgood <rmsmsgood@naver.com>
2022-04-21 10:49:19 +0900

    변이 바이러스 구현 완료

--- src/main.jl
+++ src/main.jl
@@ -13,6 +13,8 @@ function simulation(
     n_I_ = Int64[]
     n_R_ = Int64[]
     n_V_ = Int64[]
+    n_I_tier = zeros(Int64, end_time, 1)
+
 
     transmission = DataFrame(
         T = Int64[],
@@ -32,6 +34,7 @@ function simulation(
     state = fill('S', n) # using SEIR model
     LATENT = fill(-1, n)
     RECOVERY = fill(-1, n)
+    TIER = zeros(Int64, n)
     
     # LOCATION = rand(NODE_ID, n)
     LOCATION = sample(NODE_ID, Weights(data.indegree), n)
@@ -46,11 +49,13 @@ function simulation(
     state[host] .= 'I'
     LOCATION[host] .= 2935 # Wuhan, China
     RECOVERY[host] .= round.(rand(recovery_period, number_of_host)) .+ 1
+    TIER[host] .= 1
 
     coordinate = XY[:,LOCATION] + randn(2, n)
     worldmap = scatter(XY[1,:], XY[2,:], label = "airport", legend = :bottomleft)
     
-movie = @animate while T < end_time
+# movie = @animate 
+while T < end_time
     T += 1
 
     LATENT   .-= 1
@@ -59,17 +64,24 @@ movie = @animate while T < end_time
     bit_RECOVERY   = (RECOVERY .== 0)
     state[bit_LATENT  ] .= 'I'
     state[bit_RECOVERY] .= 'R'
+    TIER[bit_LATENT .&& (rand(n) .< 0.00001)] .+= 1 # Variant Virus
 
-    bit_S = (state .== 'S'); n_S = count(bit_S); push!(n_S_, n_S); ID_S = findall(bit_S)
+    bit_S = (state .== 'S'); n_S = count(bit_S); push!(n_S_, n_S);
     bit_E = (state .== 'E'); n_E = count(bit_E); push!(n_E_, n_E);
-    bit_I = (state .== 'I'); n_I = count(bit_I); push!(n_I_, n_I); ID_I = findall(bit_I)
+    bit_I = (state .== 'I'); n_I = count(bit_I); push!(n_I_, n_I);
     bit_R = (state .== 'R'); n_R = count(bit_R); push!(n_R_, n_R);
     bit_V = (state .== 'V'); n_V = count(bit_V); push!(n_V_, n_V);
+
+    while size(n_I_tier)[2] < maximum(TIER)
+        n_I_tier = [n_I_tier zeros(Int64, end_time, 1)]
+    end
+    n_I_tier[T, :] = [count(bit_I .&& (TIER .== tier)) for tier in 1:maximum(TIER)]'
 
     push!(ndws_n_, [sum(bit_I .&& (country[LOCATION] .== c)) for c in countrynames])
     if n_E + n_I == 0 break end
 
-    # println("$T: |E: $(n_E_[T]) |I: $(n_I_[T]) |R:$(n_R_[T]) |V:$(n_V_[T])")
+    println("$T: |E: $(n_E_[T]) |I: $(n_I_[T]) |R:$(n_R_[T]) |V:$(n_V_[T])")
+    println("                               maximal tier: $(maximum(TIER))")
 
     moved = (rand(n) .< σ) .&& .!bit_I
     LOCATION[moved] = rand.(NODE[LOCATION[moved]])
@@ -77,36 +89,42 @@ movie = @animate while T < end_time
     LOCATION[moved] = rand.(NODE[LOCATION[moved]])
     coordinate = XY[:,LOCATION] + randn(2, n)
 
-    frame = scatter(worldmap, coordinate[1,ID_I], coordinate[2,ID_I], label = :none,
-        markersize = 2)
+    # frame = scatter(worldmap, coordinate[1,ID_infectious], coordinate[2,ID_infectious], label = :none,
+    #     markersize = 2)
 
-    kdtreeI = KDTree(coordinate[:,ID_I])
-            
-    in_δ = inrange(kdtreeI, coordinate[:,ID_S], δ)
-    contact = length.(in_δ)
+    for tier in maximum(TIER):-1:1
+        ID_infectious = findall(bit_I .&& (TIER .== tier))
+        ID_susceptibl = findall(bit_S .|| (bit_R .&& (TIER .< tier))) # bit_V will be come
+    
+        kdtreeI = KDTree(coordinate[:,ID_infectious])
 
-    bit_infected = (rand(n_S) .< (1 .- (1 - β).^contact))
-    ID_infected = ID_S[bit_infected]
-            
-    n_infected = count(bit_infected)
-    state[ID_infected] .= 'E'
-    LATENT[ID_infected] .= round.(rand(latent_period, n_infected))
-    RECOVERY[ID_infected] .= LATENT[ID_infected] + round.(rand(recovery_period, n_infected))
+        in_δ = inrange(kdtreeI, coordinate[:,ID_susceptibl], δ)
+        contact = length.(in_δ)
+    
+        bit_infected = (rand(length(ID_susceptibl)) .< (1 .- (1 - β).^contact))
+        ID_infected = ID_susceptibl[bit_infected]
 
-    if n_infected > 0
-        from_id = ID_I[deep_pop!.(shuffle.(in_δ))[bit_infected]]
-        append!(transmission, DataFrame(
-            T = T,
-            country = country[LOCATION[ID_infected]],
-            city = city[LOCATION[ID_infected]],
-            iata = iata[LOCATION[ID_infected]],
-            from = from_id, to = ID_infected
-            ))
+        n_infected = count(bit_infected)
+        state[ID_infected] .= 'E'
+        LATENT[ID_infected] .= round.(rand(latent_period, n_infected))
+        RECOVERY[ID_infected] .= LATENT[ID_infected] + round.(rand(recovery_period, n_infected))
+        TIER[ID_infected] .= tier
+
+        if n_infected > 0
+            ID_from = ID_infectious[deep_pop!.(shuffle.(in_δ))[bit_infected]]
+            append!(transmission, DataFrame(
+                T = T,
+                country = country[LOCATION[ID_infected]],
+                city = city[LOCATION[ID_infected]],
+                iata = iata[LOCATION[ID_infected]],
+                from = ID_from, to = ID_infected
+                ))
+        end
     end
     unique!(transmission, :to)
 
     # infector_list = transmission[transmission.T .== T, :from]
-    # for defendant ∈ ID_I
+    # for defendant ∈ ID_infectious
     #     if defendant ∉ infector_list
     #         append!(non_transmission, DataFrame(
     #             T = T, t = 0, node_id = node, #x = 0, y = 0,
@@ -131,8 +149,9 @@ CSV.write("./$seed trms.csv", transmission)
 CSV.write("./$seed tevl.csv", time_evolution)
 CSV.write("./$seed ndws.csv", ndws_n_)
 CSV.write("./$seed smry.csv", summary, bom = true)
-if n_R_[T] > 1000
-    mp4(movie, "./$seed muvi.mp4", fps = 10)
-    png(plot(n_I_, label = "I"), "./$seed tevl.png")
-end
+CSV.write("./$seed tier.csv", DataFrame(n_I_tier, :auto))
+# if n_R_[T] > 1000
+#     mp4(movie, "./$seed muvi.mp4", fps = 10)
+#     png(plot(n_I_, label = "I"), "./$seed tevl.png")
+# end
 end
